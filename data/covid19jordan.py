ec9d43ad151617eea9fd8a2deda152390ea81468
osamazalaf <73951817+osamazalaf@users.noreply.github.com>
2021-04-20 14:03:02 +0300

    Add files via upload

---
+++ data/covid19jordan.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+cd4=pd.read_csv('Coviddaily.csv')
+
+
+cd=cd4.groupby('City').sum()['Confirmed'].reset_index()
+cd['OBJECTID']=cd4['OBJECTID']
+
+
+cd=cd.T
+t=list(cd.columns)
+t[0],t[1],t[2],t[3],t[4],t[5],t[6],t[7],t[8],t[9],t[10],t[11]=t[8],t[11],t[4],t[0],t[10],t[1],t[3],t[7],t[5],t[9],t[6],t[2]
+cd=cd[t]
+cd=cd.T.reset_index()
+
+cd.drop('index',axis=1,inplace=True)
+
+cd.to_csv('covidmap.csv')
+
+cd=pd.read_csv('covidmap.csv')
+
+cd.drop('Unnamed: 0',axis=1,inplace=True)
+
+cd.to_csv('covidmap.csv')
+
+cd
